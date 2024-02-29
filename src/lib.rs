83a14e112597c8fea5d76d1d68b8ec7e3e07d5bd
Nicolas Garza <nicolas.garza@gmail.com>
2024-02-29 11:22:43 -0600

    for laptop

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 use std::collections::BTreeMap;
 use serde::{Deserialize, Serialize};
-use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
 use std::thread;
 use std::fs::{self, File};
 use std::io::{self, Read};
@@ -92,86 +90,9 @@ impl KeyValueStore {
 
 // running
 pub fn run( command: String, 
-            _kv: (&Option<&str>, &Option<&str>))
+            kv: (&Option<&str>, &Option<&str>))
             -> Result<String, &'static str> {
-    match command.as_str() {
-        "start_server" => {
-            let path = Path::new("data/data.json");
-            let kv_store = KeyValueStore::load_from_file(path).unwrap();
-            let store = Arc::new(Mutex::new(kv_store));
-            match start_server(store) {
-                Ok(_) => Ok("Server started and stopped successfully".to_string()),
-                Err(_e) => Err("Server failed to start"),
-            }
-        },
-        _ => Err("This command must be run in server mode"),
-    }
-    
+
+    process(command, kv)
 }
 
-fn handle_client(mut stream: TcpStream, store: Arc<Mutex<KeyValueStore>>) {
-    let mut buffer = [0; 1024];
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(size) => {
-                if size == 0 {
-                    break;
-                }
-                let request = match std::str::from_utf8(&buffer[..size]) {
-                    Ok(req) => req,
-                    Err(_) => {
-                        eprintln!("Invalid request");
-                        return;
-                    }
-                };
-                let parts: Vec<&str> = request.trim().split_whitespace().collect();
-                if parts.len() < 2 {
-                    eprintln!("Invalid request: {}", request);
-                    return;
-                }
-
-                let command = parts[0];
-                let key = parts.get(1).copied();
-                let value = parts.get(2).copied();
-                let path = Path::new("data/data.json");
-                let mut store = store.lock().unwrap();
-
-                if command == "exit" {
-                    break;
-                }
-
-                let response = match store.process(command.to_string(), (&key, &value)) {
-                    Ok(msg) => {
-                        let _ = KeyValueStore::save(&store, path);
-                        msg
-                    },
-                    Err(e) => e.to_string(),
-                };
-
-                let _ = stream.write_all(response.as_bytes());
-            },
-            Err(e) => {
-                eprintln!("Failed to read from connection: {}", e);
-            },
-        } {}
-    }
-}
-
-fn start_server(store: Arc<Mutex<KeyValueStore>>) -> io::Result<()>{
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                let store_clone = Arc::clone(&store);
-                thread::spawn(move || {
-                    handle_client(stream, store_clone);
-                });
-            },
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
-    Ok(())
-}
