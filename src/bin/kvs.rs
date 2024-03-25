b9b43912c622b0de835f941ac60510f035e65bd0
Nicolas Garza <nicolas.garza@gmail.com>
2024-03-24 20:07:13 -0500

    :(

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -5,37 +5,37 @@ use kvs::KvStore;
 
 fn main() {
     let matches = command!()
-        .arg(Arg::new("kvs")
-            .required(true)
-            .index(1)
-            .help("Call a command on the key-value store"))
+        // .arg(Arg::new("kvs")
+        //     .required(true)
+        //     .index(1)
+        //     .help("Call a command on the key-value store"))
         .arg(
             Arg::new("command")
                 .required(true)
-                .index(2)
+                .index(1)
                 .help("Command to use on kv store"),
         )
         .arg(Arg::new("key")
-            .index(3)
+            .index(2)
             .help("Key to use in access"))
         .arg(
             Arg::new("value")
-                .index(4)
+                .index(3)
                 .help("Value to insert into kv store"),
         )
         .get_matches();
 
-    let kv_init = matches.get_one::<String>("kvs").unwrap().to_string();
+    // let kv_init = matches.get_one::<String>("kvs").unwrap().to_string();
     let kv_command = matches.get_one::<String>("command").unwrap().to_string();
     let kv_key = matches.get_one::<String>("key").map(|s| s.as_str());
     let kv_value = matches.get_one::<String>("value").map(|s| s.as_str());
 
-    let mut kv_store = KvStore::new();
+    let mut kv_store = KvStore::new(None);
 
-    if kv_init != "kvs" {
-        eprintln!("Error: incorrect arguments");
-        std::process::exit(1);
-    }
+    // if kv_init != "kvs" {
+    //     eprintln!("Error: incorrect arguments");
+    //     std::process::exit(1);
+    // }
 
     run(kv_command, (&kv_key, &kv_value), &mut kv_store);
 }
@@ -45,7 +45,9 @@ pub fn run(command: String, kv: (&Option<&str>, &Option<&str>), store: &mut KvSt
     match command.as_str() {
         "set" => {
             match store.set(kv.0.unwrap().to_string(), kv.1.unwrap().to_string()) {
-                Ok(_) => {}
+                Ok(_) => {
+                    exit(0);
+                }
                 Err(e) => {
                     eprintln!("Error: {}", e);
                     exit(1);
@@ -53,30 +55,31 @@ pub fn run(command: String, kv: (&Option<&str>, &Option<&str>), store: &mut KvSt
             }
         }
         "get" => {
-            match store.get(kv.0.unwrap().to_string()) {
-                Ok(Some(value)) => {
-                    println!("{}", value);
-                }
-                Ok(None) => {
-                    println!("Key not found");
-                }
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1);
-                }
-            }
+            // match store.get(kv.0.unwrap().to_string()) {
+            //     Ok(Some(value)) => {
+            //         println!("{}", value);
+            //     }
+            //     Ok(None) => {
+            //         println!("Key not found");
+            //     }
+            //     Err(e) => {
+            //         eprintln!("Error: {}", e);
+            //         exit(1);
+            //     }
+            // }
             exit(1);
         }
 
         "rm" => {
             match store.remove(kv.0.unwrap().to_string()) {
-                Ok(_) => {}
+                Ok(_) => {
+                    exit(0);
+                }
                 Err(e) => {
-                    eprintln!("Error: {}", e);
+                    eprintln!("{}", e);
                     exit(1);
                 }
             }
-            exit(1);
         }
 
         _ => unreachable!(),
--- src/kv.rs
+++ src/kv.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::File, io::Write, path::PathBuf};
+use std::{any, collections::HashMap, fs::{self, File, OpenOptions}, io::{BufRead, BufReader, Write}, path::{Path, PathBuf}};
 use serde::{Serialize, Deserialize};
 use crate::Result;
 
@@ -6,23 +6,30 @@ use crate::Result;
 #[derive(Debug)]
 pub struct KvStore {
     store: HashMap<String, String>,
+    path: PathBuf,
 }
 
 impl KvStore {
-    pub fn new() -> KvStore {
+    pub fn new(path: Option<&Path>) -> KvStore {
+        let path = path.map(Path::to_path_buf).unwrap_or_else(|| PathBuf::from("kvstore.log"));
         KvStore {
             store: HashMap::new(),
+            path,
         }
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        // let cmd = Command::Set(key, value);
-        // let ser = serde_json::to_string(&cmd).unwrap();
-        // let mut file = File::create("1.log")?;
-        // file.write_all(ser.as_bytes())?;
 
-        // write to the in memory hash map:
-        self.store.insert(key, value);
+        // TODO: Use a hash map as a buffer and flush it after it reaches a certain size
+        let cmd = Command::Set(key, value);
+        let ser = serde_json::to_string(&cmd).unwrap();
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(&self.path)?;
+
+        writeln!(file, "{}", ser)?;
+
         Ok(())
     }
 
@@ -32,20 +39,63 @@ impl KvStore {
     }
 
     pub fn remove(&mut self, key: String) -> Result<()> {
-        self.store.remove(&key);
-        Ok(())
+        if self.contains_key(&key) {
+            let cmd = Command::Rm(key);
+            let ser = serde_json::to_string(&cmd).unwrap();
+            let mut file = OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&self.path)?;
+            writeln!(file, "{}", ser)?;
+            Ok(())
+        } else {
+            Err(anyhow::Error::msg("Key not found"))
+        }
     }
 
-    pub fn open(_path: impl Into<PathBuf>) -> Result<KvStore> {
-        todo!()
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        let path = path.into();
+
+        fs::create_dir_all(&path)?;
+
+        Ok(KvStore {
+            store: HashMap::new(),
+            path,
+        })
+    }
+
+    fn contains_key(&self, key: &str) -> bool {
+        let file = File::open(&self.path).unwrap();
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let cmd: Command = serde_json::from_str(&line).unwrap();
+
+            match cmd {
+                Command::Set(k, _) => {
+                    if k == key {
+                        return true;
+                    }
+                }
+                Command::Rm(k) => {
+                    if k == key {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        false
     }
 }
 
 impl Default for KvStore {
     fn default() -> Self {
-        Self::new()
+        Self::new(None)
     }
 }
+
 
 #[derive(Serialize, Deserialize, Debug)]
 enum Command {
